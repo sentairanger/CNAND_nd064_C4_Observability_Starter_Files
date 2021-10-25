be41b3c626f507cacf92cbd993477ef4197119d3
sentairanger <ed2point0@gmail.com>
2021-10-25 16:46:19 -0600

    Add files via upload

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 app = Flask(__name__)
+metrics = GunicornInternalPrometheusMetrics(app)
 
+# Healthcheck status
+@app.route('/status')
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info('Status request successfull')
+    return response
+
+# Root route
 @app.route('/')
 def homepage():
     return render_template("main.html")
