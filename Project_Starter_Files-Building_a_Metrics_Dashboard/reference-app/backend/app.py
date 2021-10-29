9db91ddcc63b40d6d3ad6e4319f1f9d7e9209316
sentairanger <ed2point0@gmail.com>
2021-10-29 17:58:46 -0600

    Add files via upload

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/backend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/backend/app.py
@@ -1,75 +1,76 @@
+# import libraries
 from flask import Flask, render_template, request, jsonify, json
-import pymongo
-import os
-from flask_pymongo import PyMongo
-# Add these so that prometheus will be able to read from this data
 from prometheus_flask_exporter import PrometheusMetrics
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-# This is for flask requests
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-# This is for Gunicorn multiprocessing
-from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
+import pymongo
+from flask_pymongo import PyMongo
+from jaeger_client import Config
+from os import getenv
+import logging
 
-# set the tracer and define the tracer provider
-trace.set_tracer_provider(TracerProvider(resource=Resource.create({SERVICE_NAME: "backend-service"})))
-# Create the exporter for jaeger
-jaeger_exporter = JaegerExporter()
-
-# Add the exporter to the spanner
-spanner = BatchSpanProcessor(jaeger_exporter)
-
-# Make sure it is added here
-trace.get_tracer_provider().add_span_processor(spanner)
-
-# make sure to define the tracer here
-tracer = trace.get_tracer(__name__)
-
-# Define the app, the Prometheus Metrics and the instrumentor for Flask, also determine the server used
-
+# Define the Jaeger host which is referenced in the yaml file
+JAEGER_HOST = getenv('JAEGER_HOST', 'localhost')
+# Define the app
 app = Flask(__name__)
-FlaskInstrumentor().instrument_app(app, excluded_urls="metrics")
-gunicorn_app = "gunicorn" in os.environ.get("SERVER_SOFTWARE", "")
-if gunicorn_app:
-    metrics = GunicornInternalPrometheusMetrics(app)
-else:
-    metrics = PrometheusMetrics(app)
 
-# Define the database and the URI for MongoDB
 app.config['MONGO_DBNAME'] = 'example-mongodb'
 app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
-
 mongo = PyMongo(app)
+# Expose the metrics 
+metrics = PrometheusMetrics(app, group_by='endpoint')
+metrics.info('app_info', 'Application Info', version='1.0.3')
+# Register extra metrics
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
+    )
+)
 
-# Home route
+# Apply the same metric to all of the endpoints
+endpoint_counter = metrics.counter(
+    'endpoint_counter', 'Request count by endpoints',
+    labels={'endpoint': lambda: request.endpoint}
+)
+
+# Define the tracer
+def init_tracer(service):
+    logging.getLogger('').handlers = []
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+            'local_agent': {'reporting_host': JAEGER_HOST},
+        },
+        service_name=service,
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+tracer = init_tracer('backend')
+
 @app.route('/')
+@endpoint_counter
 def homepage():
-    with tracer.start_as_current_span('hello-world'):
-        message = "Hello world"
+    with tracer.start_span('hello-world'):
+        message = "Hello World"
     return message
 
-# API route
+
 @app.route('/api')
+@endpoint_counter
 def my_api():
-    with tracer.start_as_current_span('api'):
+    with tracer.start_span('api'):
         answer = "something"
     return jsonify(repsonse=answer)
 
-# Star route
-@app.route('/star', methods=['POST'])
-def add_star():
-  star = mongo.db.stars
-  name = request.json['name']
-  distance = request.json['distance']
-  star_id = star.insert({'name': name, 'distance': distance})
-  new_star = star.find_one({'_id': star_id })
-  output = {'name' : new_star['name'], 'distance' : new_star['distance']}
-  return jsonify({'result' : output})
-
-# health check
+# Healthcheck status
+@endpoint_counter
 @app.route('/status')
 def healthcheck():
     response = app.response_class(
@@ -80,6 +81,17 @@ def healthcheck():
     app.logger.info('Status request successfull')
     return response
 
+# This will return an error
+@app.route('/star', methods=['POST'])
+@endpoint_counter
+def add_star():
+  star = mongo.db.stars
+  name = request.json['name']
+  distance = request.json['distance']
+  star_id = star.insert({'name': name, 'distance': distance})
+  new_star = star.find_one({'_id': star_id })
+  output = {'name' : new_star['name'], 'distance' : new_star['distance']}
+  return jsonify({'result' : output})
 
 if __name__ == "__main__":
     app.run()
