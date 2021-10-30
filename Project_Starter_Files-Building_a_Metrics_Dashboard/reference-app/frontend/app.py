ce02741178b822cd9d0f3e034451a74feed8191d
sentairanger <ed2point0@gmail.com>
2021-10-29 18:00:00 -0600

    Add files via upload

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
@@ -1,22 +1,28 @@
 from flask import Flask, render_template, request, json
-from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-metrics = GunicornInternalPrometheusMetrics(app)
+metrics = PrometheusMetrics(app, group_by='endpoint')
 
-# Healthcheck status
-@app.route('/status')
-def healthcheck():
-    response = app.response_class(
-            response=json.dumps({"result":"OK - healthy"}),
-            status=200,
-            mimetype='application/json'
+metrics.info('app_info', 'Application Info', version='1.0.3')
+
+# Register extra metrics
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
     )
-    app.logger.info('Status request successfull')
-    return response
+)
+
+# Apply the same metric to all of the endpoints
+endpoint_counter = metrics.counter(
+    'endpoint_counter', 'Request count by endpoints',
+    labels={'endpoint': lambda: request.endpoint}
+)
 
 # Root route
 @app.route('/')
+@endpoint_counter
 def homepage():
     return render_template("main.html")
 
